e4d763c898b75e2d06566d012452e2a0a6ad820a
regina <reginamm@berkeley.edu>
2020-04-22 17:01:59 -0700

    added inertia

--- assets/js/script.js
+++ assets/js/script.js
@@ -80,7 +80,15 @@ $(document).ready(function() {
 
 	});
 
+	$(".inertia").click(function() {
+		$(".minocha").fadeIn(800);
+		$(".black").fadeIn(800);
+		$(".button").fadeIn(800);
+		$(".button").css("display","flex");
+		$(".button").css("justify-content","center");
+		$(".button").css("align-items","center");
 
+	});
 
 	$(".button").click(function() {
 		location.reload();
@@ -90,10 +98,10 @@ $(document).ready(function() {
 	$(".table").hover(function() {
 		$(".1").css("opacity","1");
 		$(".greenstar").show();
-		$(".greenstar").css("top",250);
+		$(".greenstar").css("top",590);
 		$(".greenstar").css("left",1105);
 		$(".lildot").show();
-		$(".lildot").css("top",66);
+		$(".lildot").css("top",400);
 		$(".lildot").css("left",610);
 
 	}, function() {
@@ -104,10 +112,10 @@ $(document).ready(function() {
 	$(".altar").hover(function() {
 		$(".2").css("opacity","1");
 		$(".bluehearts").show();
-		$(".bluehearts").css("top",420);
+		$(".bluehearts").css("top",800);
 		$(".bluehearts").css("left",1090);
 		$(".eyes").show();
-		$(".eyes").css("top",340);
+		$(".eyes").css("top",670);
 		$(".eyes").css("left",1470);
 
 	}, function() {
@@ -118,11 +126,11 @@ $(document).ready(function() {
 	$(".fruit").hover(function() {
 		$(".3").css("opacity","1");
 		$(".bigdot").show();
-		$(".bigdot").css("top",660);
+		$(".bigdot").css("top",970);
 		$(".bigdot").css("left",970);
 		$(".exclamation").show();
 		$(".exclamation").css("left",1100);
-		$(".exclamation").css("top",900);
+		$(".exclamation").css("top",1260);
 	}, function() {
 		$(".3").css("opacity","0");
 		$(".decal").hide();
@@ -132,10 +140,10 @@ $(document).ready(function() {
 		$(".4").css("opacity","1");
 		$(".yellowstars").show();
 		$(".yellowstars").css("left",1150);
-		$(".yellowstars").css("top",1240);
+		$(".yellowstars").css("top",1564);
 		$(".lildot").show();
 		$(".lildot").css("left",1610);
-		$(".lildot").css("top",1150);
+		$(".lildot").css("top",1510);
 	}, function() {
 		$(".4").css("opacity","0");
 		$(".decal").hide();
@@ -144,11 +152,11 @@ $(document).ready(function() {
 	$(".meditations").hover(function() {
 		$(".5").css("opacity","1");
 		$(".pinkstars").show();
-		$(".pinkstars").css("top",1600);
+		$(".pinkstars").css("top",1900);
 		$(".pinkstars").css("left",880);
 		$(".yeux").show();
 		$(".yeux").css("left",700);
-		$(".yeux").css("top",1250);
+		$(".yeux").css("top",1560);
 	}, function() {
 		$(".5").css("opacity","0");
 		$(".decal").hide();
@@ -158,9 +166,9 @@ $(document).ready(function() {
 		$(".6").css("opacity","1");
 		$(".exclamation").show();
 		$(".exclamation").css("left",1410);
-		$(".exclamation").css("top",1908);
+		$(".exclamation").css("top",1880);
 		$(".greenstar").show();
-		$(".greenstar").css("top",1690);
+		$(".greenstar").css("top",2080);
 		$(".greenstar").css("left",1100);
 	}, function() {
 		$(".6").css("opacity","0");
@@ -171,9 +179,9 @@ $(document).ready(function() {
 		$(".7").css("opacity","1");
 		$(".eyes").show();
 		$(".eyes").css("left",1105);
-		$(".eyes").css("top",2150);
+		$(".eyes").css("top",2440);
 		$(".bluehearts").show();
-		$(".bluehearts").css("top",1950);
+		$(".bluehearts").css("top",2200);
 		$(".bluehearts").css("left",600);
 	}, function() {
 		$(".7").css("opacity","0");
@@ -183,7 +191,7 @@ $(document).ready(function() {
 	$(".bonhomie").hover(function() {
 		$(".8").css("opacity","1");
 		$(".pinkstars").show();
-		$(".pinkstars").css("top",2183);
+		$(".pinkstars").css("top",2848);
 		$(".pinkstars").css("left",1420);
 		$(".lildot").show();
 		$(".lildot").css("top",2510);
@@ -196,11 +204,11 @@ $(document).ready(function() {
 	$(".bleed").hover(function() {
 		$(".9").css("opacity","1");
 		$(".yellowstars").show();
-		$(".yellowstars").css("top",2510);
+		$(".yellowstars").css("top",2830);
 		$(".yellowstars").css("left",995);
 		$(".yeux").show();
 		$(".yeux").css("left",740);
-		$(".yeux").css("top",2820);
+		$(".yeux").css("top",3130);
 	}, function() {
 		$(".9").css("opacity","0");
 		$(".decal").hide();
@@ -209,10 +217,10 @@ $(document).ready(function() {
 	$(".i").hover(function() {
 		$(".10").css("opacity","1");
 		$(".greenstar").show();
-		$(".greenstar").css("top",3100);
+		$(".greenstar").css("top",3400);
 		$(".greenstar").css("left",1090);
 		$(".eyes").show();
-		$(".eyes").css("top",2822);
+		$(".eyes").css("top",3130);
 		$(".eyes").css("left",1280);
 
 	}, function() {
@@ -223,14 +231,29 @@ $(document).ready(function() {
 	$(".discovering").hover(function() {
 		$(".11").css("opacity","1");
 		$(".bluehearts").show();
-		$(".bluehearts").css("top",3385);
+		$(".bluehearts").css("top",3680);
 		$(".bluehearts").css("left",1110);
 		$(".bigdot").show();
-		$(".bigdot").css("top",3300);
+		$(".bigdot").css("top",3490);
 		$(".bigdot").css("left",610);
 	}, function() {
 		$(".11").css("opacity","0");
 		$(".decal").hide();
 	});	
+
+		$(".inertia").hover(function() {
+		$(".12").css("opacity","1");
+		$(".pinkstars").show();
+		$(".pinkstars").css("top",55);
+		$(".pinkstars").css("left",1090);
+		$(".bigdot").show();
+		$(".bigdot").css("top",330);
+		$(".bigdot").css("left",1300);
+
+	}, function() {
+		$(".12").css("opacity","0");
+		$(".decal").hide();
+	});	
+
 
 });
